09e2158f15224402191521c24afaba1597e736a9
Prakhar Verma <prakharverma2019@gmail.com>
2025-06-18 05:00:39 +0530

    modified:   client/vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3001",
+        // target: "http://localhost:3001",
+        target: "https://basic-crud-app-z9e9.onrender.com",
         changeOrigin: true,
       },
     },
